d587d1b5f64e4024578fd33f94f34353d149cbc0
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2017-12-02 15:53:07 +0000

    improved the results polishing

--- Fase2/classSearchingDistsXneigh.m
+++ Fase2/classSearchingDistsXneigh.m
@@ -74,278 +74,282 @@ for iter=0:5
     x_old = x;
 end
 
+%%
+points = (unique(round(x, 3)','rows'))';
 fprintf('Number of Points:%d',length(points));
 
-%% Polishing the results without omegas 
 
-points = (unique(round(x, 3)','rows'))';
-group=cell(1,3);
 
-for i=1:60
+%% Polishing the results without omegas - only if points is less or equal than 3.
+if length(points(1,:)) <=3
+    
+    group=cell(1,3);
+
+    for i=1:60
+        for p=1:3
+            if( round(x(:,i),3) == points(:,p)) 
+                group{p}=[group{p}, i];
+            end
+        end
+    end
+
+    % Fazer cvx para as 3 classes
+
+    classPred = zeros(3);
+
     for p=1:3
-        if( round(x(:,i),3) == points(:,p)) 
-            group{p}=[group{p}, i];
-        end
+        cvx_begin quiet   
+            variable pred(3,1);
+
+            Term1=0;
+
+            for k=1:length(group{p})
+                Term1 = Term1 + (y(:, group{p}(k)) - pred)'*(y(:, group{p}(k)) - pred);
+            end
+
+            minimize(Term1);
+
+            % subject to  
+            %nothing
+        cvx_end;
+
+        pred'
+
+        %inserts point in the predictions array
+        classPred(:,p) = pred';
+
     end
-end
 
-% Fazer cvx para as 3 classes
-
-classPred = zeros(3);
-
-for p=1:3
-    cvx_begin quiet   
-        variable pred(3,1);
-  
-        Term1=0;
-
-        for k=1:length(group{p})
-            Term1 = Term1 + (y(:, group{p}(k)) - pred)'*(y(:, group{p}(k)) - pred);
-        end
-
-        minimize(Term1);
-        
-        % subject to  
-        %nothing
-    cvx_end;
-    
-    pred'
-    
-    %inserts point in the predictions array
-    classPred(:,p) = pred';
-    
-end
-
-minimum = [];
-error=[];
-for i = 1:length(classPred)
     minimum = [];
-    for j = 1:length(class)  
-        minimum =[minimum norm(classPred(:,i)-class(:,j))];
+    error=[];
+    for i = 1:length(classPred)
+        minimum = [];
+        for j = 1:length(class)  
+            minimum =[minimum norm(classPred(:,i)-class(:,j))];
+        end
+        [m mind]= min(minimum);
+        mind
+        error(i) = norm(classPred(:,i)-class(:,mind));
     end
-    [m mind]= min(minimum);
-    mind
-    error(i) = norm(classPred(:,i)-class(:,mind));
-end
 
 
-MSE=sum(error.^2)/3;
+    MSE=sum(error.^2)/3;
 
-%plot the result
-figure(1); 
-clf; 
-% printing in 2D for now
-plot(y(1,:),y(2,:),'x'); 
-hold on
-plot(x(1,:),x(2,:),'ro');
-figure(2);
-scatter3(x(1,:),x(2,:),x(3,:),'ro');
-hold on
-scatter3(y(1,:),y(2,:),y(3,:),'x');
-axis('equal'); 
-grid on;
+    %plot the result
+    figure(1); 
+    clf; 
+    % printing in 2D for now
+    plot(y(1,:),y(2,:),'x'); 
+    hold on
+    plot(x(1,:),x(2,:),'ro');
+    figure(2);
+    scatter3(x(1,:),x(2,:),x(3,:),'ro');
+    hold on
+    scatter3(y(1,:),y(2,:),y(3,:),'x');
+    axis('equal'); 
+    grid on;
+
+% Polishing Results with Omegas - only if we have more than 3 points 
+else
+
+    points = (unique(round(x, 3)','rows'))';
+    fprintf('Number of points:%d',length(points));
+
+    OmegaP = {};
+    OmegaQ = {};
+    OmegaR= {};
+
+    erromin=Inf;
+    xpmin=zeros(3,1);
+    xqmin=zeros(3,1);
+    xrmin=zeros(3,1);
 
 
-%% Polishing Results with Omegas  
+    for i=1:length(points(1,:))
+        for j=i+1:length(points(1,:))
+            for k=j+1:length(points(1,:))
+
+                % dar clear dos OmegaP e OmegaQ
+                OmegaP=[];
+                OmegaQ=[]; 
+                OmegaR=[]; 
+
+                for s=1:60 % for all the sensors
+                    %calcular a distancia entre o sensor s e todos os pontos
 
 
-points = (unique(round(x, 3)','rows'))';
-fprintf('Number of points:%d',length(points));
+                    [min_norm, min_ind] = min([norm(y(:,s) - points(:,i)) norm(y(:,s) - points(:,j)) norm(y(:,s) - points(:,k))]);
 
-OmegaP = {};
-OmegaQ = {};
-OmegaR= {};
+                    if min_ind==1
+                        OmegaP = [OmegaP , s];
+                    elseif min_ind==2
+                       OmegaQ = [OmegaQ , s];
+                    else
+                       OmegaR = [OmegaR, s];      
+                    end
 
-erromin=Inf;
-xpmin=zeros(3,1);
-xqmin=zeros(3,1);
-xrmin=zeros(3,1);
-
-
-for i=1:length(points(1,:))
-    for j=i+1:length(points(1,:))
-        for k=j+1:length(points(1,:))
-
-            % dar clear dos OmegaP e OmegaQ
-            OmegaP=[];
-            OmegaQ=[]; 
-            OmegaR=[]; 
-
-            for s=1:60 % for all the sensors
-                %calcular a distancia entre o sensor s e todos os pontos
-
-                
-                [min_norm, min_ind] = min([norm(y(:,s) - points(:,i)) norm(y(:,s) - points(:,j)) norm(y(:,s) - points(:,k))]);
-
-                if min_ind==1
-                    OmegaP = [OmegaP , s];
-                elseif min_ind==2
-                   OmegaQ = [OmegaQ , s];
-                else
-                   OmegaR = [OmegaR, s];      
                 end
 
-            end
-       
-            % obter novas posicoes de xp por minimizacao    
-            cvx_begin quiet   
-            variable xp(3, 1);
+                % obter novas posicoes de xp por minimizacao    
+                cvx_begin quiet   
+                variable xp(3, 1);
 
-            Term1=0;
+                Term1=0;
 
-            for w=1:length(OmegaP)
-                Term1 = Term1 + (y(:,OmegaP(w))-xp)'*(y(:,OmegaP(w))-xp);
-            end
-
-            
-            minimize(Term1);
-
-            cvx_end;
-
-            %nova posicao de xq
-            cvx_begin quiet   
-            variable xq(3, 1);            
-
-            Term1=0;
-
-            for w=1:length(OmegaQ)
-                Term1 = Term1 + (y(:,OmegaQ(w))-xq)'*(y(:,OmegaQ(w))-xq);
-            end
-
-            minimize(Term1);
-
-            cvx_end;
-            cvx_begin quiet   
-            variable xr(3, 1);
-
-            Term1=0;
-
-            for w=1:length(OmegaR)
-                Term1 = Term1 + (y(:,OmegaR(w))-xr)'*(y(:,OmegaR(w))-xr);
-            end
-
-            minimize(Term1);
-            cvx_end;
-            
-            
-            % calculo do erro, comparar com o previous error
-            erro=0;
-            for k=1:length(OmegaP)
-                erro = erro +(y(:,OmegaP(k))-xp)'*(y(:,OmegaP(k))-xp);
-            end
-            for k=1:length(OmegaQ)
-                erro = erro +(y(:,OmegaQ(k))-xq)'*(y(:,OmegaQ(k))-xq);
-                
-            end
-            for k=1:length(OmegaR)
-                erro = erro +(y(:,OmegaR(k))-xr)'*(y(:,OmegaR(k))-xr);
-            end
-            % se for menor, guardar apenas o i e j deste erro
-            % c.c. deixa-se estar
-            if(erro<erromin)
-                erromin=erro;
-                xpmin=xp;
-                xqmin=xq;
-                xrmin=xr;
-            end
-        end
-  
-    end
-end
-
-%agora voltar a inserir os 3 valores obtidos para obter melhores
-%estimativas
-
-            % dar clear dos OmegaP e OmegaQ
-            OmegaP=[];
-            OmegaQ=[]; 
-            OmegaR=[]; 
-
-            for s=1:60 % for all the sensors
-                %calcular a distancia entre o sensor s e todos os pontos
-         
-                [min_norm, min_ind] = min([norm(y(:,s) - xpmin) norm(y(:,s) - xqmin) norm(y(:,s) - xrmin)]);
-
-                if min_ind==1
-                    OmegaP = [OmegaP , s];
-                elseif min_ind==2
-                   OmegaQ = [OmegaQ , s];
-                else
-                   OmegaR = [OmegaR, s];      
+                for w=1:length(OmegaP)
+                    Term1 = Term1 + (y(:,OmegaP(w))-xp)'*(y(:,OmegaP(w))-xp);
                 end
 
+
+                minimize(Term1);
+
+                cvx_end;
+
+                %nova posicao de xq
+                cvx_begin quiet   
+                variable xq(3, 1);            
+
+                Term1=0;
+
+                for w=1:length(OmegaQ)
+                    Term1 = Term1 + (y(:,OmegaQ(w))-xq)'*(y(:,OmegaQ(w))-xq);
+                end
+
+                minimize(Term1);
+
+                cvx_end;
+                cvx_begin quiet   
+                variable xr(3, 1);
+
+                Term1=0;
+
+                for w=1:length(OmegaR)
+                    Term1 = Term1 + (y(:,OmegaR(w))-xr)'*(y(:,OmegaR(w))-xr);
+                end
+
+                minimize(Term1);
+                cvx_end;
+
+
+                % calculo do erro, comparar com o previous error
+                erro=0;
+                for k=1:length(OmegaP)
+                    erro = erro +(y(:,OmegaP(k))-xp)'*(y(:,OmegaP(k))-xp);
+                end
+                for k=1:length(OmegaQ)
+                    erro = erro +(y(:,OmegaQ(k))-xq)'*(y(:,OmegaQ(k))-xq);
+
+                end
+                for k=1:length(OmegaR)
+                    erro = erro +(y(:,OmegaR(k))-xr)'*(y(:,OmegaR(k))-xr);
+                end
+                % se for menor, guardar apenas o i e j deste erro
+                % c.c. deixa-se estar
+                if(erro<erromin)
+                    erromin=erro;
+                    xpmin=xp;
+                    xqmin=xq;
+                    xrmin=xr;
+                end
             end
-       
-            % obter novas posicoes de xp por minimizacao    
-            cvx_begin quiet   
-            variable xp(3, 1);
 
-            Term1=0;
-
-            for w=1:length(OmegaP)
-                Term1 = Term1 + (y(:,OmegaP(w))-xp)'*(y(:,OmegaP(w))-xp);
-            end
-
-            
-            minimize(Term1);
-
-            cvx_end;
-
-            %nova posicao de xq
-            cvx_begin quiet   
-            variable xq(3, 1);            
-
-            Term1=0;
-
-            for w=1:length(OmegaQ)
-                Term1 = Term1 + (y(:,OmegaQ(w))-xq)'*(y(:,OmegaQ(w))-xq);
-            end
-
-            minimize(Term1);
-
-            cvx_end;
-            cvx_begin quiet   
-            variable xr(3, 1);
-
-            Term1=0;
-
-            for w=1:length(OmegaR)
-                Term1 = Term1 + (y(:,OmegaR(w))-xr)'*(y(:,OmegaR(w))-xr);
-            end
-
-            minimize(Term1);
-            cvx_end;
-            
-xpmin=xp;
-xqmin=xq;
-xrmin=xr;
-
-% error 
-minimum = [];
-classPred=[xpmin xqmin xrmin];
-error=[];
-
-minimum = [];
-error=[];
-for i = 1:length(classPred)
-    minimum = [];
-    for j = 1:length(class)  
-        minimum =[minimum norm(classPred(:,i)-class(:,j))];
+        end
     end
-    [m mind]= min(minimum);
-    error(i) = norm(classPred(:,i)-class(:,mind));
+
+    %agora voltar a inserir os 3 valores obtidos para obter melhores
+    %estimativas
+
+                % dar clear dos OmegaP e OmegaQ
+                OmegaP=[];
+                OmegaQ=[]; 
+                OmegaR=[]; 
+
+                for s=1:60 % for all the sensors
+                    %calcular a distancia entre o sensor s e todos os pontos
+
+                    [min_norm, min_ind] = min([norm(y(:,s) - xpmin) norm(y(:,s) - xqmin) norm(y(:,s) - xrmin)]);
+
+                    if min_ind==1
+                        OmegaP = [OmegaP , s];
+                    elseif min_ind==2
+                       OmegaQ = [OmegaQ , s];
+                    else
+                       OmegaR = [OmegaR, s];      
+                    end
+
+                end
+
+                % obter novas posicoes de xp por minimizacao    
+                cvx_begin quiet   
+                variable xp(3, 1);
+
+                Term1=0;
+
+                for w=1:length(OmegaP)
+                    Term1 = Term1 + (y(:,OmegaP(w))-xp)'*(y(:,OmegaP(w))-xp);
+                end
+
+
+                minimize(Term1);
+
+                cvx_end;
+
+                %nova posicao de xq
+                cvx_begin quiet   
+                variable xq(3, 1);            
+
+                Term1=0;
+
+                for w=1:length(OmegaQ)
+                    Term1 = Term1 + (y(:,OmegaQ(w))-xq)'*(y(:,OmegaQ(w))-xq);
+                end
+
+                minimize(Term1);
+
+                cvx_end;
+                cvx_begin quiet   
+                variable xr(3, 1);
+
+                Term1=0;
+
+                for w=1:length(OmegaR)
+                    Term1 = Term1 + (y(:,OmegaR(w))-xr)'*(y(:,OmegaR(w))-xr);
+                end
+
+                minimize(Term1);
+                cvx_end;
+
+    xpmin=xp;
+    xqmin=xq;
+    xrmin=xr;
+
+    % error 
+    minimum = [];
+    classPred=[xpmin xqmin xrmin];
+    error=[];
+
+    error=[];
+    for i = 1:length(classPred)
+        minimum = [];
+        for j = 1:length(class)  
+            minimum =[minimum norm(classPred(:,i)-class(:,j))];
+        end
+        [m mind]= min(minimum);
+        mind
+        error(i) = norm(classPred(:,i)-class(:,mind));
+    end
+
+
+    MSE=sum(error.^2)/3;
+
+
+    figure(4);
+    %plot the resuts
+    hold on;
+    scatter3(xrmin(1,:),xrmin(2,:),xrmin(3,:),'ro');
+    scatter3(xqmin(1,:),xqmin(2,:),xqmin(3,:),'ro');
+    scatter3(xpmin(1,:),xpmin(2,:),xpmin(3,:),'ro');
+    scatter3(y(1,:),y(2,:),y(3,:),'x');
+    axis('equal'); 
+    grid on;
 end
-
-
-MSE=sum(error.^2)/3;
-
-
-figure(4);
-%plot the resuts
-hold on;
-scatter3(xrmin(1,:),xrmin(2,:),xrmin(3,:),'ro');
-scatter3(xqmin(1,:),xqmin(2,:),xqmin(3,:),'ro');
-scatter3(xpmin(1,:),xpmin(2,:),xpmin(3,:),'ro');
-scatter3(y(1,:),y(2,:),y(3,:),'x');
-axis('equal'); 
-grid on;
