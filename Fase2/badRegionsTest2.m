4007c0c3961eacb28acfe74b9ee20ad97c5d8631
Einscrew <manel.rodrigo.reis@gmail.com>
2017-12-03 21:24:23 +0000

    graphics

--- Fase2/badRegionsTest2.m
+++ Fase2/badRegionsTest2.m
@@ -4,6 +4,15 @@ class3 = [[2.5 0 0];[0 2.5 0];[1.85 1.85 0];];
 class4 = [[2.5 0 0];[0 2.5 0];[1.75 1.75 0];];
 class5 = [[2.5 0 0];[0 2.5 0];[1.5 1.5 0];];
 class6 = [[2.5 0 0];[0 2.5 0];[1.25 1.25 0];];
+class7 = [[2.5 0 0];[0 2.5 0];[2.5 2.25 0];];
+class8 = [[2.5 0 0];[0 2.5 0];[2.5 2 0];];
+class9 = [[2.5 0 0];[0 2.5 0];[2.5 1.75 0];];
+class10 = [[2.5 0 0];[0 2.5 0];[2.5 1.5 0];];
+class11 = [[2.5 0 0];[0 2.5 0];[2.5 1.25 0];];
+class12 = [[2.5 0 0];[0 2.5 0];[2.5 1 0];];
+class13 = [[2.5 0 0];[0 2.5 0];[2.5 0.75 0];];
+class14 = [[2.5 0 0];[0 2.5 0];[2.5 0.5 0];];
+class15 = [[2.5 0 0];[0 2.5 0];[2.5 0.25 0];];
 
 classes = cell(3,1);
 classes{1} = class1';
@@ -12,13 +21,22 @@ classes{3} = class3';
 classes{4} = class4';
 classes{5} = class5';
 classes{6} = class6';
+classes{7} = class7';
+classes{8} = class8';
+classes{9} = class9';
+classes{10} = class10';
+classes{11} = class11';
+classes{12} = class12';
+classes{13} = class13';
+classes{14} = class14';
+classes{15} = class15';
 
 %% 
-rVector = randn(3,60);
-classtype = fix(rand(60,1)*3)+1;
+%rVector = randn(3,60);
+%classtype = fix(rand(60,1)*3)+1;
 %%
 factor = 0.85;
-ONLYPRINT = 1;
+ONLYPRINT = 0;
 %%
 c = ['r','b','g'];
 ro = 2;
@@ -34,7 +52,8 @@ for i = 1:length(classes)
     %%
     if ONLYPRINT == 0
     [classPred, x, mse, points]=funcXneighbors(y, neig, ro, classes{i});
-    [ind, ctr] = kmeans(y, 3);
+    opts = statset('Display','final');
+    [ind, ctr] = kmeans(y',3,'Distance','cityblock','Replicates',5,'Options',opts);
     %%
     C{i} = ctr;
     CLASSPred{i} = classPred;
